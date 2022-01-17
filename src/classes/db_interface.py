67032f118d616540f43da2c11cb4e242e9512dcc
Yoann Renard <yoann.rnrd@gmail.com>
2022-01-17 21:18:10 +0100

    db fix

--- src/classes/db_interface.py
+++ src/classes/db_interface.py
@@ -2,9 +2,9 @@ import pymongo
 import os
 class db_interface:
     def __init__(self):
-        database_url = os.environ("DATABASE_URL")
-        db_name = os.environ("DB_NAME")
-        collection_name = os.environ("COLLECTION_NAME")
+        database_url = os.environ["DATABASE_URL"]
+        db_name = os.environ["DB_NAME"]
+        collection_name = os.environ["COLLECTION_NAME"]
 
         self.client = pymongo.MongoClient(database_url) # connection to MongoDB
         self.database = self.client[db_name]  # select MongoDB's database
--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
-
+from classes.db_interface import db_interface
 app = FastAPI()
 
-
+mongo = db_interface()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/insert")
+async def insert():
+    mongo.insert_publication(
+        {
+            "test": 1
+        }
+    )
+    return {"message": "publication posted"}
