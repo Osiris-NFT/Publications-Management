744a212386570a928284e44f8a6c498268966e93
root <root@localhost.localdomain>
2022-01-18 22:57:08 +0100

    db_interface update del comment et ajout del reply

--- src/classes/db_interface.py
+++ src/classes/db_interface.py
@@ -3,7 +3,7 @@ import os
 from bson import ObjectId
 class db_interface:
     def __init__(self):
-        """database_url = os.environ["DATABASE_URL"]
+        """database_url = os.environ["DATABASE_URL"]    #get every values of env
         db_name = os.environ["DB_NAME"]
         collection_name = os.environ["COLLECTION_NAME"]"""
         #self.key_list = {'','',''}
@@ -20,29 +20,29 @@ class db_interface:
         #else:
         self.collection.insert_one(publication)
 
-    def delete_one_publication(self, publication_id: str) -> None:
+    def delete_one_publication(self, publication_id: str) -> None: #NOT TESTED
         self.collection.delete_one({'_id': ObjectId(publication_id)})
 
-    def delete_one_comment(self, comment_id: str, publication_id: str = '') -> None:
-        if publication_id: # If there is a publication id to help the query
-            self.collection.find_one_and_update(
-                {"_id": ObjectId(publication_id)},
-                {
-                    "$pull":{
-                        "comment":{
-                            "_id": ObjectId(comment_id)
-                        }
+    def delete_one_comment(self, comment_id: str, publication_id: str) -> None: #NOT TESTED
+        self.collection.find_one_and_update(
+            {"_id": ObjectId(publication_id)},
+            {
+                "$pull":{
+                    "comments":{
+                        "_id": ObjectId(comment_id)
                     }
                 }
-            )
-        else: # If there isn't :(
-            self.collection.find_one_and_update(
-                {},
-                {
-                    "$pull": {
-                        "comment": {
-                            "_id": ObjectId(comment_id)
-                        }
+            }
+        )
+
+    def delete_one_reply(self, reply_id: str, publication_id: str) -> None:#NOT TESTED
+        self.collection.find_one_and_update(
+            {"_id": ObjectId(publication_id)},
+            {
+                "$pull": {
+                    "comments.replies": {
+                        "_id": ObjectId(reply_id)
                     }
                 }
-            )
+            }
+        )
