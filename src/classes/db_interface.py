9ac25b2ddd25e909e7e2ca32ef34532ca02dda55
root <root@localhost.localdomain>
2022-01-18 18:56:12 +0100

    ca marche

--- src/classes/db_interface.py
+++ src/classes/db_interface.py
@@ -2,13 +2,13 @@ import pymongo
 import os
 class db_interface:
     def __init__(self):
-        database_url = os.environ["DATABASE_URL"]
+        """database_url = os.environ["DATABASE_URL"]
         db_name = os.environ["DB_NAME"]
-        collection_name = os.environ["COLLECTION_NAME"]
+        collection_name = os.environ["COLLECTION_NAME"]"""
 
-        self.client = pymongo.MongoClient(database_url) # connection to MongoDB
-        self.database = self.client[db_name]  # select MongoDB's database
-        self.collection = self.database[collection_name] # select database's Collection
+        self.client = pymongo.MongoClient("mongodb://127.0.0.1:27017/") # connection to MongoDB
+        self.database = self.client["publications-service"]  # select MongoDB's database
+        self.collection = self.database["publications"] # select database's Collection
 
     def insert_publication(self, publication: dict) -> None:
         #if publication[''] and publication[''] # format check
