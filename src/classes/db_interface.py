09491348bc751374139e6e478169b533991eed42
root <root@localhost.localdomain>
2022-01-19 18:50:51 +0100

    api update

--- src/classes/db_interface.py
+++ src/classes/db_interface.py
@@ -8,22 +8,17 @@ class db_interface:
         """database_url = os.environ["DATABASE_URL"]    #get every values of env
         db_name = os.environ["DB_NAME"]
         collection_name = os.environ["COLLECTION_NAME"]"""
-        #self.key_list = {'','',''}
 
         self.client = pymongo.MongoClient("mongodb://127.0.0.1:27017/") # connection to MongoDB
         self.database = self.client["publications-service"]  # select MongoDB's database
         self.collection = self.database["publications"] # select database's Collection
 
-    def insert_one_publication(self, publication: dict) -> None:
-        #pub_keys = publication.keys()      #Check dict validity
-        #for key in self.key_list:
-        #   if key is not in pub_keys:
-        #       return Exception
-        #else:
-        self.collection.insert_one(publication)
+    def insert_one_publication(self, publication: dict) -> str:
+        result = self.collection.insert_one(publication)
         print("\nPublication:")
         pprint(publication)
         print("inserted in database")
+        return str(result.inserted_id)
 
     def add_one_comment(self, comment: dict, publication_id: str) -> None:
         #self.collection.find_one_and_update()
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from classes.db_interface import db_interface
 from bson import ObjectId
 from datetime import datetime
@@ -16,7 +16,7 @@ class publicationModel(BaseModel): #_id, timestamps, hashtags and likes count ha
     media_url: str
     category: "photography" or "pixel-art" or "digital-drawing"
 
-class commentModel(BaseModel):  #_id, timestamps, hashtags and likes count have to be built by the service afterward
+class commentModel(BaseModel):  #_id, timestamps and likes count have to be built by the service afterward
     user: str
     content: str
 
@@ -28,13 +28,104 @@ class replyModel(BaseModel):
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Publication service is alive !"}
 
+
+
+def stringifyIDs(publication: dict) -> dict:
+    """Stringify every ObjectId() of a mongoDB's publication"""
+    publication['_id'] = str(publication['_id'])
+    for x in range(len(publication["comments"])):
+        publication['comments'][x]['_id'] = str(publication['comments'][x]['_id'])
+        for y in range(len(publication['comments'][x]['replies'])):
+            publication['comments'][x]['replies'][y]['_id'] = str(publication['comments'][x]['replies'][y]['_id'])
+    return publication
+
+"""
+@app.post("/post_publication", status_code = status.HTTP_201_CREATED, response_model=publicationModel)
+async def post_publication(publication: publicationModel):
+    publication["_id"] = ObjectId()
+    publication["publication_date"] = datetime.now()
+    publication_id = mongodb_interface.insert_one_publication(publication)
+    publication["_id"] = publication_id
+    return {
+        "message": "Publication posted !",
+        "publication": publication
+    }
+"""
+@app.get("/insert_samples") # DEBUG
+async def insert_sample():
+    for publication in samples:
+        mongodb_interface.insert_one_publication(publication)
+    return {"message": "samples posted"}
+
+
+@app.get("/get_publication_by_id/{publication_id}", status_code = status.HTTP_200_OK)
+async def get_publication(publication_id: str, response: Response):
+    if not ObjectId.is_valid(publication_id):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            "message": "Invalid ID"
+        }
+
+    publication = mongodb_interface.get_one_publication(publication_id)
+    if publication != None:
+        formated_publication = stringifyIDs(publication)
+        return {
+            "publication": formated_publication
+        }
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {
+            "message": "publication does not exist"
+        }
+
+
+
+@app.get("/get_publications_of_one_user/{user_name}", status_code = status.HTTP_200_OK)
+async def get_user_publications(user_name: str, response: Response):
+    publications = mongodb_interface.get_user_publications(user_name)
+    if publications != []:
+        formated_publications = []
+        for publication in publications:
+            formated_publications.append(stringifyIDs(publication))
+        return {
+            "message": "publications returned",
+            "publications": formated_publications
+        }
+    else:
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return {
+            "message": "user have no publications"
+        }
+
+
+@app.delete("/delete/{publication_id}", status_code = status.HTTP_204_NO_CONTENT)
+async def delete_publication(publication_id: str ,response: Response):
+    if not ObjectId.is_valid(publication_id):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return {
+            "message": "Invalid ID"
+        }
+
+    removed_publication = mongodb_interface.delete_one_publication(publication_id)
+    if removed_publication != None:
+        formated_publication = stringifyIDs(removed_publication)
+        return {
+            "message": f"publication {publication_id} deleted",
+            "removed publication": formated_publication
+        }
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {
+            "message": "No publication to delete"
+        }
+        
 
 samples = [
     {
         "_id": ObjectId(),
-        "plublication_date": str(datetime.now()),
+        "publication_date": str(datetime.now()),
         "publication_name": "sample 1",
         "user_name": "user 1",
         "content_type": "image",
@@ -73,7 +164,7 @@ samples = [
     },
     {
         "_id": ObjectId(),
-        "plublication_date": str(datetime.now()),
+        "publication_date": str(datetime.now()),
         "publication_name": "sample 2",
         "user_name": "Ayaya",
         "content_type": "image",
@@ -84,35 +175,35 @@ samples = [
         "likes_count": 203,
         "comments":[
             {
-                    "_id": ObjectId(),
-                    "user": "jokesterdu13",
-                    "plublication_date": str(datetime.now()),
-                    "content": "where the cat lol",
-                    "likes_count": 10,
-                    "replies": [
-                        {
-                            "_id": ObjectId(),
-                            "user": "Ayaya",
-                            "plublication_date": str(datetime.now()),
-                            "target_user": "jokesterdu13",
-                            "content": "ahah",
-                            "likes_count": 5
-                        },
-                        {
-                            "_id": ObjectId(),
-                            "user": "ImBored",
-                            "plublication_date": str(datetime.now()),
-                            "target_user": "Ayaya",
-                            "content": "Not impressed, maybe you meant 'haha' ?",
-                            "likes_count": 0
-                        }
-                    ]
+                "_id": ObjectId(),
+                "user": "jokesterdu13",
+                "plublication_date": str(datetime.now()),
+                "content": "where the cat lol",
+                "likes_count": 10,
+                "replies": [
+                    {
+                        "_id": ObjectId(),
+                        "user": "Ayaya",
+                        "plublication_date": str(datetime.now()),
+                        "target_user": "jokesterdu13",
+                        "content": "ahah",
+                        "likes_count": 5
+                    },
+                    {
+                        "_id": ObjectId(),
+                        "user": "ImBored",
+                        "plublication_date": str(datetime.now()),
+                        "target_user": "Ayaya",
+                        "content": "Not impressed, maybe you meant 'haha' ?",
+                        "likes_count": 0
+                    }
+                ]
             }
         ]
     },
     {
         "_id": ObjectId(),
-        "plublication_date": str(datetime.now()),
+        "publication_date": str(datetime.now()),
         "publication_name": "sample 3",
         "user_name": "Mamamia",
         "content_type": "image",
@@ -167,44 +258,3 @@ samples = [
         ]
     }
 ]
-
-@app.post("/post_publication")
-async def post_publication(publication: publicationModel):
-    pass
-
-@app.get("/insert_samples") # DEBUG
-async def insert():
-    for publication in samples:
-        mongodb_interface.insert_one_publication(publication)
-    return {"message": "publications posted"}
-
-
-@app.get("/get/{publication_id}")
-async def get_publication(publication_id: str):
-    publication = mongodb_interface.get_one_publication(publication_id)
-    publication['_id'] = str(publication['_id'])
-    for x in range(len(publication["comments"])):
-        publication['comments'][x]['_id'] = str(publication['comments'][x]['_id'])
-        for y in range(len(publication['comments'][x]['replies'])):
-            publication['comments'][x]['replies'][y]['_id'] = str(publication['comments'][x]['replies'][y]['_id'])
-    return {
-        "publication": publication
-    }
-
-
-@app.delete("/delete/{publication_id}")
-async def delete_publication(publication_id):
-    removed_publication = mongodb_interface.delete_one_publication(publication_id)
-    if removed_publication != None:
-        return {
-            "message": f"publication {publication_id} deleted",
-            "removed publication": removed_publication
-        }
-    else:
-        response = {
-            "message": f"publication {publication_id} deleted",
-            "removed publication": removed_publication
-        }
-        response.status_code = 201
-        return response
-        
