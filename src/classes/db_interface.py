2eac5449a0e5e69f28cefd49a7b344f6eff0e182
Yoann-Renard <yoann.rnrd@gmail.com>
2022-02-24 15:55:28 +0100

    deployment update

--- src/classes/db_interface.py
+++ src/classes/db_interface.py
@@ -5,18 +5,18 @@ from pprint import pprint
 from bson import ObjectId
 class db_interface:
     def __init__(self):
-        """database_url = os.environ["DATABASE_URL"]    #get every values of env
+        database_url = os.environ["DATABASE_URL"]    #get every values of env
         db_name = os.environ["DB_NAME"]
         collection_name = os.environ["COLLECTION_NAME"]
 
         self.client = pymongo.MongoClient(database_url) # connection to MongoDB
         self.database = self.client[db_name]  # select MongoDB's database
         self.collection = self.database[collection_name] # select database's Collection
-        """
+        
         #DEBUG
-        self.client = pymongo.MongoClient("mongodb://127.0.0.1:27017/") # connection to MongoDB
+        """self.client = pymongo.MongoClient("mongodb://127.0.0.1:27017/") # connection to MongoDB
         self.database = self.client["publications-service"]  # select MongoDB's database
-        self.collection = self.database["publications"] # select database's Collection
+        self.collection = self.database["publications"] # select database's Collection"""
 
     def insert_one_publication(self, publication: dict) -> str:
         result = self.collection.insert_one(publication)
